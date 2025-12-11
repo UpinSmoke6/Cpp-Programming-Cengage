434e4896c6d6113188bb817b455be87f7d01c9ea
Derek Sathre <164694886+UpinSmoke6@users.noreply.github.com>
2025-12-11 02:03:23 -0600

    Add files via upload

---
+++ InputStatement/InputStatement.cpp
@@ -0,0 +1,32 @@
+// Thi s program illustrates how input statements work in C++.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int feet;
+	int inches;
+
+	cout << "Enter two digits separated by a space : ";
+	cin >> feet >> inches;
+	cout << endl;
+
+	cout << "Feet = " << feet << endl;
+	cout << "Inches = " << inches << endl;
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
